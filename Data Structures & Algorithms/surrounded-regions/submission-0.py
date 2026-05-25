8139f8fb944765694ec5ad4730c3aee642c3c854
KrishRamoliya <krishramoliya434@gmail.com>
2026-05-25 21:50:49 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        rows , cols  = len(board) , len(board[0])
+
+        #capture unsurrounded area
+        def capture(r,c):
+
+            if r < 0 or c < 0 or r>=rows or c>=cols or board[r][c]!='O':
+                return 
+            board[r][c]='T'
+            capture(r+1,c)
+            capture(r,c+1)
+            capture(r-1,c)
+            capture(r,c-1)
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col]=='O' and(row in [0,rows-1] or col in [0,cols-1]):
+                    capture(row, col) 
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col]=='O':
+                    board[row][col]='X'
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col]=='T':
+                    board[row][col]='O'
+        
